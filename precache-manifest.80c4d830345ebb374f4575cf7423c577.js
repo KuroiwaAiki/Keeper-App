f3473c1e32797b706f131b93dc473deb6de595be
James Mummery <Jamie@Jamess-MacBook-Air.local>
2020-05-30 18:29:21 -0700

    Updates

---
+++ precache-manifest.80c4d830345ebb374f4575cf7423c577.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6de29b809a2bbee41cb9f5e50ad2ff1c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7dfd7df55dc6d45dea0c",
+    "url": "/static/js/2.133928f0.chunk.js"
+  },
+  {
+    "revision": "1260a01290ac0c0454e9a7627323041c",
+    "url": "/static/js/2.133928f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6ea0e39a5718a0334fa",
+    "url": "/static/js/main.510bddb4.chunk.js"
+  },
+  {
+    "revision": "c7e029b0735d742cc720",
+    "url": "/static/js/runtime-main.0d5d714b.js"
+  }
+]);
